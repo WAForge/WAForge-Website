943aff643b7ac41c92f7ec78aa90c59dbf6462fd
deactiveasdf <111084408+destructiveasdf@users.noreply.github.com>
2024-05-02 22:34:23 -0700

    Update Form.jsx

--- WAForgeSite/src/assets/Form.jsx
+++ WAForgeSite/src/assets/Form.jsx
@@ -1,39 +1,41 @@
+import { useState } from 'react';
+
 const Form = () => {
-    function Submit(e){
-        const formEle = document.querySelector('.form');
-        e.preventDefault();
-        const formDatab = new FormData(formEle);
-        fetch("https://script.google.com/macros/s/AKfycbxpUSe8hW86h6jdIa3t5TXWirMO78rLAAjKu2R7sEuJphqBZqbPX3mjbqA_RS2BgF7A/exec",       
-        {
-            method: "POST",
-            body: formDatab
-        })
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error(`HTTP error! status: ${res.status}`);
-            }
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-            }
+    const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch('https://script.google.com/macros/s/AKfycbyN4vcGZWsnrMGhBKyjMwHrxOeOqihu55LOWgPxmVKK1OEVorwPztXLsaA_pnhe45nEcQ/exec', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ name, email })
+})
+.then(() => {
+  console.log(JSON.stringify({ name, email }));
+})
+.catch(error => console.log('Error:', error));
+  };
+
+    
     return ( 
         <div className="flex flex-col items-center justify-center h-fit text-white">
             <div className="max-w-lg p-6 rounded-lg shadow-lg">
-                <form className="form" onSubmit={(e)=>Submit(e)}>
+                <form className="form" onSubmit={handleSubmit}>
                 <input
                 type="text"
                 placeholder="Enter your name"
                 className="w-full px-4 py-1 mb-4 bg-[#00000000] border-2 border-[#00dc82] rounded-lg focus:outline-none focus:border-teal-500"
+                onChange={e => setName(e.target.value)}
                 />
                 <input
                 type="email"
                 placeholder="Enter your email address"
                 className="w-full px-4 py-1 mb-4 bg-[#00000000] border-2 border-[#00dc82] rounded-lg focus:outline-none focus:border-teal-500"
+                onChange = {e => setEmail(e.target.value)}
                 />
                 <button
                 type="submit"
