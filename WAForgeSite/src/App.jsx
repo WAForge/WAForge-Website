a193c839f1e72e5641612d6c45fef9d79fd2df41
deactiveasdf <111084408+destructiveasdf@users.noreply.github.com>
2024-05-03 19:10:29 -0700

    fasdf

--- WAForgeSite/src/App.jsx
+++ WAForgeSite/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import React, { useEffect } from 'react';
 import Header from './assets/Header'
 import VideoSection from './assets/VideoSection';
 import SectionBreak from './assets/SectionBreak';
@@ -15,25 +16,32 @@ import Form from './assets/Form';
 import SocialMedias from './assets/SocialMedia';
 import Person from './assets/Pictures/person.jpg';
 import Footer from  './assets/Footer';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const serviceSection = useRef();
 
+  const [count, setCount] = useState(0)
+  const ref = useRef(null)
+  const doSomething = () => {
+    ref.current?.scrollIntoView({behavior: 'smooth'});
+  }
   return (
     <>
       <div className="bg-[#191516] h-[100vh] w-[100vw] overflow-x-hidden">
             <Header />
-            <VideoSection videos = {[video1, video2, video3]} isHero = {true}/>
+            <VideoSection videos = {[video1, video2, video3]} isHero = {true} element={<button onClick={() => doSomething()} className='relative inline-flex items-center justify-center p-0.5 mt-10 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-[12.5px] group bg-gradient-to-br from-[#00dc82] via-[#36e4da] to-[#16a79e] group-hover:from-green-400 duration-[0.1s] hover:shadow-[0_0px_15px_0px_rgba(0,255,130,1)] dark:text-white '><span className='font-Montserrat font-light relative transition-all ease-in duration-75 bg-white dark:bg-[#18181b] rounded-[9.5px] px-10 py-2'>Get Started</span></button>}/>
             <SectionBreak />
+
             <ServiceSection id="services" title="SERVICES" 
               element1={
-                <div className='flex flex-col justify-center items-center'>
-                  <ImageHorizontalCard subTitle={<CardSubtitle />} extraStyle = " h-[600px] w-[90vw] md:h-[350px] md:w-[1200px]" Element1 = {<Button link = "https://waforge.org" text = "Sign Up" paddingx = {" px-[2.5rem]"} paddingy = " py-[0.35rem]" extraStyle="mt-[3vh]"/>} image = {hackathon} title="HACKATHON" description="WAForge offers annual hackathons for students to showcase their skills and learn from industry professionals. These hackathons offer grand cash prizes and the opportunity to network with tech enthusiasts." />
-                  <ImageHorizontalCard extraStyle=" h-[650px] w-[90vw] md:h-[450px] md:w-[1200px]" subTitle={<CardSubtitle />} Element1={<Form />} title="NEWSLETTER" description="Join our newsletter to stay up to date with the latest news and events in tech and business. Our newsletter also offers exclusive content and updates on upcoming events and activities at WAForge." />
+                <div ref={ref} className='flex flex-col justify-center items-center'>
+                  <ImageHorizontalCard subTitle={<CardSubtitle />} extraStyle = " h-[600px] w-[90vw] md:h-[350px] md:w-[1200px]" Element1 = {<Button isOpenDiv={true} text = "Sign Up" paddingx = {" px-[2.5rem]"} paddingy = " py-[0.35rem]" extraStyle="mt-[3vh] font-[500]"/>} image = {hackathon} title="HACKATHON" description="WAForge offers annual hackathons for students to showcase their skills and learn from industry professionals. These hackathons offer grand cash prizes and the opportunity to network with tech enthusiasts." />
+                  <ImageHorizontalCard extraStyle=" h-[650px] w-[90vw] md:h-[450px] md:w-[1200px]" subTitle={<CardSubtitle />} Element1={<Form paddingx="px-10" paddingy="py-2"/>} title="NEWSLETTER" description="Join our newsletter to stay up to date with the latest news and events in tech and business. Our newsletter also offers exclusive content and updates on upcoming events and activities at WAForge." />
                   <ImageHorizontalCard title="COMING SOON" extraStyle = " h-[600px] w-[90vw] md:h-[370px] md:w-[1200px]" description="Follow us on our social media to stay updated on everything WAForge!" Element2={<Button link = "https://hcb.hackclub.com/donations/start/waforge" text = "Support WAForge" paddingx = {" px-[4rem]"} paddingy = " py-[0.5rem]" extraStyle="mt-[0vh]"/>} Element1={<SocialMedias />} />
 
                 </div>
-            
+
             }
               
               />
@@ -70,32 +78,32 @@ function App() {
             <ImageHorizontalCard title="TEAM" extraStyle = " h-[1500px] w-[90vw] md:h-[700px] md:w-[1200px]" Element1 = {
               <div>
                 <div className='flex flex-col md:flex-row justify-center items-center'>
+                  <div className='w-[90%] md:w-fit m-4 mx-[3.2rem] flex flex-col justify-center items-center'>
+                    <img src={Person} alt="WAForge Team" className="h-[150px] w-[150px] rounded-[50%] border-[2px] border-[#00ffc8]"/>
+                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >Siddarth Tangutur</h1>
+                    <p className="text-white text-[1em] font-[200] font-Montserrat mt-[7px]">Co-Founder</p>
+                  </div>
                   <div className='w-[90%] md:w-fit m-4 mx-20 flex flex-col justify-center items-center'>
                     <img src={Person} alt="WAForge Team" className="h-[150px] w-[150px] rounded-[50%] border-[2px] border-[#00ffc8]"/>
-                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >John Doe</h1>
+                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >Rahul Peesa</h1>
                     <p className="text-white text-[1em] font-[200] font-Montserrat mt-[7px]">Founder</p>
                   </div>
                   <div className='w-[90%] md:w-fit m-4 mx-20 flex flex-col justify-center items-center'>
                     <img src={Person} alt="WAForge Team" className="h-[150px] w-[150px] rounded-[50%] border-[2px] border-[#00ffc8]"/>
-                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >John Doe</h1>
-                    <p className="text-white text-[1em] font-[200] font-Montserrat mt-[7px]">Founder</p>
-                  </div>
-                  <div className='w-[90%] md:w-fit m-4 mx-20 flex flex-col justify-center items-center'>
-                    <img src={Person} alt="WAForge Team" className="h-[150px] w-[150px] rounded-[50%] border-[2px] border-[#00ffc8]"/>
-                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >John Doe</h1>
-                    <p className="text-white text-[1em] font-[200] font-Montserrat mt-[7px]">Founder</p>
+                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >Evan Gubarev</h1>
+                    <p className="text-white text-[1em] font-[200] font-Montserrat mt-[7px]">Technology Manager</p>
                   </div>
                 </div>
                 <div className='flex flex-col md:flex-row justify-center items-center'>
                   <div className='w-[90%] md:w-fit m-4 mx-20 flex flex-col justify-center items-center'>
                     <img src={Person} alt="WAForge Team" className="h-[150px] w-[150px] rounded-[50%] border-[2px] border-[#00ffc8]"/>
-                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >John Doe</h1>
-                    <p className="text-white text-[1em] font-[200] font-Montserrat mt-[7px]">Founder</p>
+                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >Arnav Koganti</h1>
+                    <p className="text-white text-[1em] font-[200] font-Montserrat mt-[7px]">Outreach Manager</p>
                   </div>
                   <div className='w-[90%] md:w-fit m-4 mx-20 flex flex-col justify-center items-center'>
                     <img src={Person} alt="WAForge Team" className="h-[150px] w-[150px] rounded-[50%] border-[2px] border-[#00ffc8]"/>
-                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >John Doe</h1>
-                    <p className="text-white text-[1em] font-[200] font-Montserrat mt-[7px]">Founder</p>
+                    <h1 className="text-white font-Montserrat font-[500] tracking-[0.2em] text-[1.2em] md:text-[1.2em] textGlow mt-[15px]" >Nitin Munugeti</h1>
+                    <p className="text-white text-[1em] font-[200] font-Montserrat mt-[7px]">Marketing Manager</p>
                   </div>
 
                 
--- WAForgeSite/src/assets/Form.jsx
+++ WAForgeSite/src/assets/Form.jsx
@@ -1,52 +1,19 @@
-import { useState } from 'react';
+import { PopupButton } from '@typeform/embed-react'
+import React, { useState } from 'react';
+ 
+ 
+const Form = (props) => {
+  const { link, action, text, paddingx, paddingy, extraStyle } = props;
+  const [isHovered, setIsHovered] = useState(false);
+  const buttonClasses = "font-Montserrat mt-10 relative inline-flex items-center justify-center p-0.5 mt-5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-[12.5px] group bg-gradient-to-br from-[#00dc82] via-[#36e4da] to-[#16a79e] group-hover:from-green-400 hover:shadow-[0_0px_15px_0px_rgba(0,255,130,1)] group-hover:shadow-white duration-[0.1s] dark:text-white " + extraStyle;
 
-const Form = () => {
-    const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const spanClasses = "relative transition-all ease-in duration-75 bg-white dark:bg-[#18181b] rounded-[9.5px] font-[400] " + paddingx + " " + paddingy;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    fetch('https://script.google.com/macros/s/AKfycbyN4vcGZWsnrMGhBKyjMwHrxOeOqihu55LOWgPxmVKK1OEVorwPztXLsaA_pnhe45nEcQ/exec', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ name, email })
-})
-.then(() => {
-  console.log(JSON.stringify({ name, email }));
-})
-.catch(error => console.log('Error:', error));
-  };
-
-    
-    return ( 
-        <div className="flex flex-col items-center justify-center h-fit text-white">
-            <div className="max-w-lg p-6 rounded-lg shadow-lg">
-                <form className="form" onSubmit={handleSubmit}>
-                <input
-                type="text"
-                placeholder="Enter your name"
-                className="w-full px-4 py-1 mb-4 bg-[#00000000] border-2 border-[#00dc82] rounded-lg focus:outline-none focus:border-teal-500"
-                onChange={e => setName(e.target.value)}
-                />
-                <input
-                type="email"
-                placeholder="Enter your email address"
-                className="w-full px-4 py-1 mb-4 bg-[#00000000] border-2 border-[#00dc82] rounded-lg focus:outline-none focus:border-teal-500"
-                onChange = {e => setEmail(e.target.value)}
-                />
-                <button
-                type="submit"
-                className="w-full px-4 py-1 border-[#00dc82] border-2 text-[#ffffff] font-Montserrat rounded-lg focus:outline-none transition-all duration-2 hover:text-black hover:bg-[#00dc82]"
-                >
-                Submit
-                </button>
-                </form>
-            </div>
-    </div>
-     );
+  return (
+      <PopupButton id="dn0SdXrn" className={buttonClasses}>
+          <span className={spanClasses}>Sign Up</span>
+      </PopupButton>
+  )
 }
  
 export default Form;
